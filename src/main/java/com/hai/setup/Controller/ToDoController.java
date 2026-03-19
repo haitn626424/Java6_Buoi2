03d8ca4e970d408f6ef9fbbe4f9494bc66f9e059
KiemThuiNangCao <chaoban10102006@gmail.com>
2026-03-19 14:04:16 +0700

    Update

--- src/main/java/com/hai/setup/Controller/ToDoController.java
+++ src/main/java/com/hai/setup/Controller/ToDoController.java
@@ -1,42 +1,61 @@
 package com.hai.setup.Controller;
-
 import com.hai.setup.Entity.ToDo;
-import com.hai.setup.Repository.ToDoRepository;
 import com.hai.setup.Service.ToDoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 @RestController
 @RequestMapping("/api/todos")
 @RequiredArgsConstructor
 public class ToDoController {
+
     private final ToDoService toDoService;
+
     //api/todos
     @GetMapping
-    public List<ToDo> findAll(){
-        return toDoService.findAll();
+    public ResponseEntity<List<ToDo>>  findAll(){
+        List<ToDo> todos = toDoService.findAll();
+
+        // return new ResponseEntity<>(todos, HttpStatus.OK);
+
+        // return ResponseEntity.ok(todos);
+
+         return ResponseEntity.status(HttpStatus.OK).body(todos);
+
+//        return ResponseEntity
+//                .ok()
+//                .header("Custom-Header","Custom-Value").body(todos);
     }
     // GET: api/todos/{id}
     @GetMapping("/{id}")
-    public ToDo getToDo(@PathVariable long id){
-        return toDoService.findById(id);
+    public ResponseEntity<ToDo>  getToDo(@PathVariable long id){
+       ToDo todos =  toDoService.findById(id);
+       return  new ResponseEntity<>(todos, HttpStatus.OK);
     }
 
     // POS: api/todos
+    // 201-CREATED
     @PostMapping
-    public ToDo add(@RequestBody ToDo toDo){
-        return  toDoService.add(toDo);
+    public ResponseEntity<ToDo> add(@RequestBody ToDo toDo){
+        ToDo addTodo = toDoService.add(toDo);
+        return new ResponseEntity<>(addTodo, HttpStatus.CREATED);
     }
     // PUT: api/todos/{id}
     @PutMapping("/{id}")
-    public ToDo update(@RequestBody ToDo toDo, @PathVariable long id){
-        return toDoService.update(toDo,id);
+    public ResponseEntity<ToDo> update(@RequestBody ToDo toDo, @PathVariable long id){
+         ToDo updateTodo = toDoService.update(toDo,id);
+         //return new ResponseEntity<>(updateTodo, HttpStatus.OK);
+        return ResponseEntity
+                .ok()
+                .header("Custome-Name","Custome-Value").body(updateTodo);
+
     }
     // DELETE: api/todos/{id}
     @DeleteMapping("/{id}")
-    public ToDo delete(@PathVariable long id){
-        return toDoService.delete(id);
+    public ResponseEntity<ToDo> delete(@PathVariable long id){
+       ToDo deleteTodo = toDoService.delete(id);
+       return new ResponseEntity<>(deleteTodo, HttpStatus.OK);
     }
 }
